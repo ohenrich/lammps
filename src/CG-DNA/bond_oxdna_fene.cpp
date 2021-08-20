611e2212e9569bfb4ab9561f201d847722f6567e
Lewis M Russell <71833891+lrussell676@users.noreply.github.com>
2021-08-20 13:33:55 +0100

    src

--- src/CG-DNA/bond_oxdna_fene.cpp
+++ src/CG-DNA/bond_oxdna_fene.cpp
@@ -56,7 +56,6 @@ BondOxdnaFene::~BondOxdnaFene()
 /* ----------------------------------------------------------------------
     compute vector COM-sugar-phosphate backbone interaction site in oxDNA
 ------------------------------------------------------------------------- */
-
 void BondOxdnaFene::compute_interaction_sites(double e1[3], double /*e2*/[3],
   double /*e3*/[3], double r[3])
 {
@@ -158,9 +157,7 @@ void BondOxdnaFene::ev_tally_xyz(int i, int j, int nlocal, int newton_bond,
 ------------------------------------------------------------------------- */
 void BondOxdnaFene::compute(int eflag, int vflag)
 {
-  printf("\n \nFrom bond_oxdna_fene.cpp:");
   int a,b,in,type;
-  double bb_sum[6];
   double delf[3],delta[3],deltb[3]; // force, torque increment;;
   double delr[3],ebond,fbond;
   double rsq,Deltasq,rlogarg;
@@ -173,7 +170,6 @@ void BondOxdnaFene::compute(int eflag, int vflag)
 
   double **x = atom->x;
   double **f = atom->f;
-  double **bb_pos = atom->bb_pos;
   double **torque = atom->torque;
 
   AtomVecEllipsoid *avec = (AtomVecEllipsoid *) atom->style_match("ellipsoid");
@@ -204,22 +200,8 @@ void BondOxdnaFene::compute(int eflag, int vflag)
     // vector COM-backbone site a and b
     compute_interaction_sites(ax,ay,az,ra_cs);
     compute_interaction_sites(bx,by,bz,rb_cs);
-	
-	printf("\n \n ra_cs = %f %f %f", ra_cs[0], ra_cs[1], ra_cs[2]);
-	printf("\n rb_cs = %f %f %f", rb_cs[0], rb_cs[1], rb_cs[2]);
 
     // vector backbone site b to a
-	
-	printf("\n bb_pos_a = %f %f %f, \n  a = %d", bb_pos[a][0], bb_pos[a][1], bb_pos[a][2], a);
-	printf("\n bb_pos_b = %f %f %f, \n  b = %d", bb_pos[b][0], bb_pos[b][1], bb_pos[b][2], b);
-	bb_sum[0] = x[a][0] + ra_cs[0];
-	bb_sum[1] = x[a][1] + ra_cs[1];
-	bb_sum[2] = x[a][2] + ra_cs[2];
-	bb_sum[3] = x[b][0] + rb_cs[0];
-	bb_sum[4] = x[b][1] + rb_cs[1];
-	bb_sum[5] = x[b][2] + rb_cs[2];
- 	printf("\n bb_pos_a = %f %f %f", bb_sum[0], bb_sum[1], bb_sum[2]);
-	printf("\n bb_pos_b = %f %f %f", bb_sum[3], bb_sum[4], bb_sum[5]);
 	
     delr[0] = x[a][0] + ra_cs[0] - x[b][0] - rb_cs[0];
     delr[1] = x[a][1] + ra_cs[1] - x[b][1] - rb_cs[1];
