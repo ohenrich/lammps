ee761697d14560ad1d81f87d0389a1c191e4a002
Kierran Falloon <kgwfalloon@gmail.com>
2024-04-25 18:29:28 +0100

    oxRNA2 potential file reading and real units
    For RNA, d_cs_x is treated as d_cs within ConstantsOxdna in order to reduce code duplication and complexity.

--- src/CG-DNA/constants_oxdna.h
+++ src/CG-DNA/constants_oxdna.h
@@ -35,6 +35,13 @@ class ConstantsOxdna : protected Pointers {
   static double get_lambda_dh_one_prefactor() { return lambda_dh_one_prefactor; }
   static double get_qeff_dh_pf_one_prefactor() { return qeff_dh_pf_one_prefactor; }
 
+  // oxRNA 2 getters
+  static double get_d_cs_z() { return d_cs_z; }
+  static double get_d_cst_x_3p() { return d_cst_x_3p; }
+  static double get_d_cst_y_3p() { return d_cst_y_3p; }
+  static double get_d_cst_x_5p() { return d_cst_x_5p; }
+  static double get_d_cst_y_5p() { return d_cst_y_5p; }
+
  private:
   std::string units;
   bool real_flag;
@@ -46,6 +53,11 @@ class ConstantsOxdna : protected Pointers {
   // oxDNA 2 parameters
   static double d_cs_x, d_cs_y;
   static double lambda_dh_one_prefactor, qeff_dh_pf_one_prefactor;
+
+  // oxRNA 2 parameters
+  static double d_cs_z;
+  static double d_cst_x_3p, d_cst_y_3p;
+  static double d_cst_x_5p, d_cst_y_5p;
 };
 
 }    // namespace LAMMPS_NS
