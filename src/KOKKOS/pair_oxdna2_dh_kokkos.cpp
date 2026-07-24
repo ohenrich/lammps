229f451e63ae02ca4c69f82fb5db36028e9c43c6
Lewis M Russell <lewis.russell@strath.ac.uk>
2026-07-24 15:48:48 +0100

    vendor/backend safe

--- src/KOKKOS/pair_oxdna2_dh_kokkos.cpp
+++ src/KOKKOS/pair_oxdna2_dh_kokkos.cpp
@@ -357,7 +357,7 @@ void PairOxdna2DhKokkos<DeviceType>::operator()(TagPairOxdna2DhCompute<OXDNAFLAG
 
     if (rsq > d_cutsq_dh_c(atype, btype)) continue; // Note the switch of sign, > vs <=, due to using continue
 
-    rinv = rsqrtf(rsq);
+    rinv = static_cast<KK_FLOAT>(Kokkos::rsqrt(rsq));
     r = rsq * rinv;
 
     if (r <= d_cut_dh_ast(atype, btype)) {
